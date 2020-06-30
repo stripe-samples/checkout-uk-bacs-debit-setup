4a4b485a4174f27c5cfeb179210fa900462ec857
Matthew Ling <matthewling@stripe.com>
2020-06-30 17:28:07 +0100

    Initial commit

---
+++ client/html/script.js
@@ -0,0 +1,46 @@
+// The max and min number of photos a customer can purchase
+var MIN_PHOTOS = 1;
+var MAX_PHOTOS = 10;
+
+/* Handle any errors returns from Checkout  */
+var handleResult = function (result) {
+  if (result.error) {
+    var displayError = document.getElementById('error-message');
+    displayError.textContent = result.error.message;
+  }
+};
+
+// Create a Checkout Session with the selected quantity
+var createCheckoutSession = function () {
+  return fetch('/create-checkout-session', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      locale: i18next.language.toLowerCase().split('-')[0],
+    }),
+  }).then(function (result) {
+    return result.json();
+  });
+};
+
+/* Get your Stripe publishable key to initialize Stripe.js */
+fetch('/config')
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (json) {
+    window.config = json;
+    var stripe = Stripe(config.publicKey);
+    // Setup event handler to create a Checkout Session on submit
+    document.querySelector('#submit').addEventListener('click', function (evt) {
+      createCheckoutSession().then(function (data) {
+        stripe
+          .redirectToCheckout({
+            sessionId: data.sessionId,
+          })
+          .then(handleResult);
+      });
+    });
+  });
